d2481367e3d14f8296863a931e2f513c7359990d
Cristal-LRN1 <cristal.loreto.rn@gmail.com>
2024-11-29 13:08:19 -0600

    Este es mi código comentado y corregido 3

--- filtro.js
+++ filtro.js
@@ -6,18 +6,21 @@ const productos = [
   {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
   {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+];
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const li = document.getElementsById("lista-de-productos");//se corrige la estructura del metodo para que sea get.ElementById para agregar el id
+const $i = document.querySelector('input'); //se corrige la const con un document.query.Selector para la clase input
 
+
+//Se cambia la funcion para mostrar los productos
+const mostrarProductos = (productos) => {
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+  var d = document.createElement("div");
+  d.classList.add("producto");
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+  var ti = document.createElement("p");
+  ti.classList.add("titulo");
+  ti.textContent = productos[i].nombre;
   
   var imagen = document.createElement("img");
   imagen.setAttribute('src', productos[i].img);
@@ -28,7 +31,11 @@ for (let i = 0; i < productos.length; i++) {
   li.appendChild(d)
 }
 
-displayProductos(productos)
+};
+
+//Se llama a la funcion para mostrar los productos
+
+mostrarProductos(productos);
 const botonDeFiltro = document.querySelector("button");
 
 botonDeFiltro.onclick = function() {
@@ -38,24 +45,16 @@ botonDeFiltro.onclick = function() {
 
   const texto = $i.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
 
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
-    
-    var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
+  //Se modifica a la funcion para mostrar los productos y evitar errores en el código
+
+  if (texto === ""){
+    mostrarProductos(productos);
+    return;
   }
+
+  const productosFiltrados = filtrado(productos, texto);
+  mostrarProductos(productosFiltrados);
 }
 
 const filtrado = (productos = [], texto) => {
